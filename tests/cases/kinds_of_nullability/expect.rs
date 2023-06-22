9df37684349d07ac4e0f69d86be9e2aac1a3e1bb
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2023-06-22 12:23:58 +0200

    add test/example of the different kinds of nullability and their implications

---
+++ tests/cases/kinds_of_nullability/expect.rs
@@ -0,0 +1,28 @@
+type NotNullableAndRequired = i64;
+type NotNullableAndNotRequired = i64;
+type MaybeNullableAndRequired = Option<NullableAndRequired>;
+type NullableAndRequired = i64;
+type MaybeNullableAndNotRequired = Option<NullableAndNotRequired>;
+///note that this produces an `Option<Option<_>>`
+type NullableAndNotRequired = i64;
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    openapi_gen::reexport::serde::Serialize,
+    openapi_gen::reexport::serde::Deserialize,
+    Copy,
+    Eq,
+    Hash,
+    openapi_gen::reexport::derive_more::Constructor
+)]
+pub struct Foo {
+    pub not_nullable_and_required: NotNullableAndRequired,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub not_nullable_and_not_required: Option<NotNullableAndNotRequired>,
+    pub nullable_and_required: MaybeNullableAndRequired,
+    ///note that this produces an `Option<Option<_>>`
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nullable_and_not_required: Option<MaybeNullableAndNotRequired>,
+}
+
