bb1c9af9d68dac29dec19b84eca275fa7abce102
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2023-06-23 16:29:03 +0200

    add test case for extensible enums

---
+++ tests/cases/x_extensible_enum/expect.rs
@@ -0,0 +1,15 @@
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    openapi_gen::reexport::serde::Serialize,
+    openapi_gen::reexport::serde::Deserialize,
+    Eq,
+    Hash
+)]
+pub enum DeliveryMethod {
+    Parcel,
+    Letter,
+    Email,
+    Other(String)
+}
