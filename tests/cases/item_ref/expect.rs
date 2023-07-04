29b98888319b739d43eb093fb8adb00e50197f10
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2023-07-04 16:50:07 +0200

    add a test about deduplicating item refs

---
+++ tests/cases/item_ref/expect.rs
@@ -0,0 +1,28 @@
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    openapi_gen::reexport::serde::Serialize,
+    openapi_gen::reexport::serde::Deserialize,
+    Copy,
+    Eq,
+    Hash,
+    openapi_gen::reexport::derive_more::Constructor
+)]
+pub struct Id(pub openapi_gen::reexport::uuid::Uuid);
+type Foo = f64;
+type Bar = String;
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    openapi_gen::reexport::serde::Serialize,
+    openapi_gen::reexport::serde::Deserialize,
+    openapi_gen::reexport::derive_more::Constructor
+)]
+pub struct Thing {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub foo: Option<Foo>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub bar: Option<Bar>,
+}
